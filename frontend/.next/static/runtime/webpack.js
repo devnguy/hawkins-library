7836c55853aa7bcfb105665390ef02d9e355e3ba
Devin Nguyen <hdn301@gmail.com>
2020-02-18 09:37:06 -0800

    added _document.js

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f6781dabcc43911aff0b";
+/******/ 	var hotCurrentHash = "cd051ed35fb734383052";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
