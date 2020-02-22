4e04069a80478f0a42ce183dfd1d362415c275ac
Devin Nguyen <hdn301@gmail.com>
2020-02-21 20:16:07 -0800

    db interaction through api

--- pages/api/get-books.js
+++ pages/api/get-books.js
@@ -5,7 +5,8 @@ export default function getBooks(req, res) {
   console.log(req.query);
   console.log(req.cookies);
   
-  mysql.pool.query(`SELECT * FROM books`, (err, rows, fields) => {
-    res.send(rows);
-  })
+  // mysql.pool.query(`SELECT * FROM books`, (err, rows, fields) => {
+  //   res.send(rows);
+  // })
+  res.send('hello world')
 }
