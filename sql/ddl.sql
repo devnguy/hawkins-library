be18c9ee155913fc7c60fc4440875f8a59646b50
Rebekah <koonr@oregonstate.edu>
2020-02-28 13:15:32 -0800

    fixed dml file

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,13 +1,11 @@
-/** 
-  * Authors: Huy Nguyen, Rebekah Koon
-  * Description: Data Definition Queries for Hawkins Library. 
-  *     Creates tables that will be used in our library
-  *     database as well as queries for inserting data into
-  *     the tables.
-  */
-​
--- Creating the customers table, which will list the
--- information of customers who are library members.
+-- Authors: Huy Nguyen, Rebekah Koon
+-- Description: Data Definition Queries for Hawkins Library. 
+--    Creates tables that will be used in our library
+--   database as well as queries for inserting data into
+--   the tables.
+
+-- Creating the customers table, which will list the information of customers who are library members.
+DROP TABLE IF EXISTS `customers`;
 CREATE TABLE `customers` (
     PRIMARY KEY (`customerId`),
     `customerId` int(11) NOT NULL AUTO_INCREMENT,
@@ -20,9 +18,9 @@ CREATE TABLE `customers` (
     `lateFee` int(11) NOT NULL
         DEFAULT 0
 );
-​
--- Creating the events table, which will list upcoming
--- library events.
+
+-- Creating the events table, which will list upcoming library events.
+DROP TABLE IF EXISTS `events`;
 CREATE TABLE `events` (
     PRIMARY KEY (`eventId`),
     `eventId` int(11) NOT NULL AUTO_INCREMENT,
@@ -32,10 +30,10 @@ CREATE TABLE `events` (
     `description` varchar(255),
     `imgUrl` varchar(255)
 );
-​
+
 -- Creating the eventRegistrations table, which will list
--- the customer IDs of customers who have signed up for
--- library events.
+-- the customer IDs of customers who have signed up for library events.
+DROP TABLE IF EXISTS `eventRegistrations`;
 CREATE TABLE `eventRegistrations` (
     PRIMARY KEY (`registrationId`),
     `registrationId` int(11) NOT NULL AUTO_INCREMENT,
@@ -44,11 +42,12 @@ CREATE TABLE `eventRegistrations` (
     `eid` int(11) NOT NULL,
         FOREIGN KEY(`eid`) REFERENCES `events` (`eventId`),
         CONSTRAINT `uniqueRegistration` 
-            UNIQUE (`cid`, `eid`)
+        UNIQUE (`cid`, `eid`)
 );
-​
+
 -- Creating the checkoutOrders table, which will list
 -- library orders that customers have made.
+DROP TABLE IF EXISTS `checkoutOrders`;
 CREATE TABLE `checkoutOrders` (
     PRIMARY KEY (`orderId`),
     `orderId` INT(11) NOT NULL AUTO_INCREMENT,
@@ -57,9 +56,10 @@ CREATE TABLE `checkoutOrders` (
     `checkoutDate` DATE NOT NULL,
     `dueDate` DATE NOT NULL
 );
-​
+
 -- Creating the books table, which will list information of
 -- books held at the library.
+DROP TABLE IF EXISTS `books`;
 CREATE TABLE `books` (
     PRIMARY KEY (`bookId`),
     `bookId` INT(11) NOT NULL AUTO_INCREMENT,
@@ -71,38 +71,37 @@ CREATE TABLE `books` (
     `genre` VARCHAR(255) NOT NULL,
     `imgUrl` VARCHAR(255)
 );
-​
-​
+
 -- Inserting customer data into customers table.
-INSERT INTO `customers` (`firstName`, `lastName`, `email`, `phone`,) 
-VALUES  ('Sara', 'Smith', 'smithsara@hello.com', '19700102'), 
-        ('Miguel', 'Cabrera', 'cm@hello.com', '19880202'), 
-        ('Bo', 'Chang', 'bochang@hello.com', '19850302');
-​
+INSERT INTO `customers` (`firstName`, `lastName`, `email`, `phone`) 
+VALUES ('Sara', 'Smith', 'smithsara@hello.com', '19700102'), 
+       ('Miguel', 'Cabrera', 'cm@hello.com', '19880202'), 
+(      'Bo', 'Chang', 'bochang@hello.com', '19850302');
+        
 -- Inserting event data into events table.
 INSERT INTO `events` (`name`, `date`, `guest`, `description`, `imgUrl`) 
 VALUES  ('Children''s Reading Club', 20200315, 'Ananya Jaiswal', 'At our monthly reading clubs, pairs of volunteers read aloud to small groups of children, while their parents receive training on how to encourage their children to read at home.', '/features/feature01.jpeg'), 
         ('Book Talk & Signing with George R.R. Martin', 20200317, 'George R.R. Martin', 'George R.R. Martin is an American novelist and short story writer. He wrote the series of epic fantasy novels, A Song of Ice and Fire, which was adapted into the HBO series Game of Thrones', '/features/feature02.jpeg'), 
         ('Lecture and Book Signing with J.K. Rowling', 20200320, 'J.K. Rowling', 'J.K. Rowling is a British author and philanthropist. She is best known for writing the Harry Potter fantasy series, which has won multiple awards and sold more than 500 million copies, becoming the best-selling book series in history.', '/features/feature03.jpeg'), 
         ('Tuesday and Thursday Night Tutoring at Hawkins', 20200320, 'H.P. Lovecraft', 'Led by the most talented and experienced educators in the key academic disciplines. All educators are highly qualified classroom teachers who share our philosophy of learning.', '/features/feature04.jpeg');
-​
+    
 -- Inserting event registrations data into eventRegistrations table, which
 -- will show library customers who have signed up for each event.
 INSERT INTO `eventRegistrations` (`cid`, `eid`) 
 VALUES 
-        ((SELECT customerId FROM customers WHERE email = "smithsara@hello.com"), 
-        (SELECT eventId FROM events WHERE eventId = 11)),
-        ((SELECT customerId FROM customers WHERE email = "smithsara@hello.com"), 
-        (SELECT eventId FROM events WHERE eventId = 12)),
-        ((SELECT customerId FROM customers WHERE firstName = "cm@hello.com"), 
-        (SELECT eventId FROM events WHERE eventId = 11)),
-        ((SELECT customerId FROM customers WHERE firstName = "cm@hello.com"), 
-        (SELECT eventId FROM events WHERE eventId = 13)),
-        ((SELECT customerId FROM customers WHERE email = "bochang@hello.com"), 
-        (SELECT eventId FROM events WHERE eventId = 12)),
-        ((SELECT customerId FROM customers WHERE email = "bochang@hello.com"), 
-        (SELECT eventId FROM events WHERE eventId = 11));
-​
+        ((SELECT customerId FROM customers WHERE email = 'smithsara@hello.com'), 
+        (SELECT eventId FROM events WHERE eventId = 1)),
+        ((SELECT customerId FROM customers WHERE email = 'smithsara@hello.com'), 
+        (SELECT eventId FROM events WHERE eventId = 2)),
+        ((SELECT customerId FROM customers WHERE email = 'cm@hello.com'), 
+        (SELECT eventId FROM events WHERE eventId = 1)),
+        ((SELECT customerId FROM customers WHERE email = 'cm@hello.com'), 
+        (SELECT eventId FROM events WHERE eventId = 3)),
+        ((SELECT customerId FROM customers WHERE email = 'bochang@hello.com'), 
+        (SELECT eventId FROM events WHERE eventId = 2)),
+        ((SELECT customerId FROM customers WHERE email = 'bochang@hello.com'), 
+        (SELECT eventId FROM events WHERE eventId = 1));
+        
 -- Inserting checkout orders that customers have made into checkoutOrders table.
 INSERT INTO `checkoutOrders` (`cid`, `checkoutDate`, `dueDate`) 
 VALUES 
@@ -110,7 +109,7 @@ VALUES
         "2020-02-15", "2020-03-15"),
         ((SELECT `customerId` FROM `customers` WHERE email = "bochang@hello.com"), 
         "2020-02-17", "2020-03-17");
-​
+        
 -- Inserting books into the Books table.
 INSERT INTO `books` (`title`, `author`, `publisher`, `genre`, `oid`, `imgUrl`) 
 VALUES
