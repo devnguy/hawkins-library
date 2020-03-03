034b013ece816d53b9777b26d61aeca73d6ba9a6
Devin Nguyen <hdn301@gmail.com>
2020-03-02 16:39:20 -0800

    error handling

--- pages/api/books/get-return-books.js
+++ pages/api/books/get-return-books.js
@@ -2,26 +2,36 @@ const db = require('../../../lib/db')
 const escape = require('sql-template-strings')
 
 module.exports = async (req, res) => {
-  const checkedOutBooks = await db.query(escape`
-    SELECT bookId, title, author, oid, imgUrl, email
-    FROM books
-    INNER JOIN checkoutOrders ON checkoutOrders.orderId = books.oid
-    INNER JOIN customers ON customers.customerId = checkoutOrders.cid
+  const queryForEmail = await db.query(escape`
+    SELECT email
+    FROM customers
     WHERE email = ${req.body.email}
   `)
-  //   console.log('hello from the server')
-  //   if (checkedOutBooks.error) {
-  //     console.log('hello from the server')
-  //     res.status(500).json(checkedOutBooks.error)
-  //   } else {
-  //     res.status(200).json(checkedOutBooks)
-  //   }
-  const numberOfBooks = checkedOutBooks.length
-  res.status(200).json({
-    message: `${numberOfBooks} books found under ${req.body.email}`,
-    statusNo: 0,
-    checkedOutBooks,
-    numberOfBooks,
-    userEmail: req.body.email
-  })
+  if (queryForEmail.length === 0) {
+    res.status(200).json({
+      message: 'Email not found.',
+      statusNo: 3,
+      checkedOutBooks: [],
+      numberOfBooks: 0,
+      userEmail: req.body.email
+    })
+  } else {
+    const checkedOutBooks = await db.query(escape`
+      SELECT bookId, title, author, oid, imgUrl, email
+      FROM books
+      INNER JOIN checkoutOrders ON checkoutOrders.orderId = books.oid
+      INNER JOIN customers ON customers.customerId = checkoutOrders.cid
+      WHERE email = ${req.body.email}
+    `)
+    const numberOfBooks = checkedOutBooks.length
+    res.status(200).json({
+      message: `${numberOfBooks} book${numberOfBooks != 1 ? 's' : ''} found under ${
+        req.body.email
+      }`,
+      statusNo: 0,
+      checkedOutBooks,
+      numberOfBooks,
+      userEmail: req.body.email
+    })
+  }
 }
