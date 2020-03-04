bf6010f835eca69282673bb5419817aec51515d6
Devin Nguyen <hdn301@gmail.com>
2020-03-03 17:43:12 -0800

    render input fields on icon click

---
+++ pages/api/customers/update-customer.js
@@ -0,0 +1,20 @@
+const db = require('../../../lib/db')
+const escape = require('sql-template-strings')
+
+module.exports = async (req, res) => {
+  const updateCustomer = await db.query(escape`
+    UPDATE customers
+    SET firstName = ${req.body.firstName},
+        lastName = ${req.body.lastName},
+        email = ${req.body.email},
+        phone = ${req.body.phone}
+    WHERE customerId = ${req.body.id}
+  `)
+
+  const customers = await db.query(escape`
+    SELECT *
+    FROM customers
+  `)
+
+  res.status(200).json(customers)
+}
