34b547918155f22484303e1444833e7f39762fcd
Devin Nguyen <hdn301@gmail.com>
2020-04-07 17:32:26 -0500

    aws

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   env: {
-    MYSQL_HOST: '35.247.96.250',
-    MYSQL_USER: 'root',
-    MYSQL_DATABASE: 'hawkins',
+    MYSQL_HOST: 'hawkins.csxzaeqzyuwc.us-west-1.rds.amazonaws.com',
+    MYSQL_USER: 'admin',
+    MYSQL_DATABASE: 'library',
     MYSQL_PASSWORD: 'eleven11',
     DEV_ENDPOINT: 'http://localhost:3000',
-    PROD_ENDPOINT: 'https://hawkins-library.now.sh'
-  }
+    PROD_ENDPOINT: 'https://hawkins-library.now.sh',
+  },
 }
